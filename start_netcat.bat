cbdc38110c6aa36849e7d5a6454b490f9e78fd7e
unknown <ers@THE_RAIN.(none)>
2017-04-09 01:29:25 +0300

    First Commit

---
+++ start_netcat.bat
@@ -0,0 +1,5 @@
+cd c:/netcat
+nc64 -l -p 5000 | mplayer -fps 31 -cache 1024 -
+echo Raspberry pi cmd: netcat480p.sh
+echo Raspberry pi cmd: netcat720p.sh
+echo To stop the video: CTRL+Z, ps,  kill -9 PID  
